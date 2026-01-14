542df26fd0b9566b29b518befdbd8680759b69b6
Mohammed Ihsaan <mohammedihsaan28@gmail.com>
2026-01-14 20:30:33 +0530

    Implement Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+'use client';
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
       <main>
         <Navbar/>
+        <Hero/>
       </main>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Image from "next/image";
+import { useEffect, useRef } from "react";
+
+export default function Hero() {
+
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+
+    useEffect(() => {
+        if(videoRef.current){
+            videoRef.current.playbackRate = 2;
+        }
+
+    },[]);
+
+    return(
+        <section id="hero">
+            <div>
+                <h1>MacBook Pro</h1>
+                <Image src={'/images/title.png'} alt="Title" width={700} height={500}/>
+            </div>
+
+            <video ref={videoRef} src={'/videos/hero.mp4'} autoPlay muted playsInline/>
+
+            <button>Buy</button>
+
+            <p className="mb-2">From $1999 or $133/mo for 12 months</p>
+
+        </section>
+    )
+}
