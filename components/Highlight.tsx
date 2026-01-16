19572839a9f25615b1f8423188a82e5ec2e0b115
Mohammed Ihsaan <mohammedihsaan28@gmail.com>
2026-01-16 19:35:37 +0530

    Implement Footer section

--- components/Highlight.tsx
+++ components/Highlight.tsx
@@ -34,12 +34,12 @@ export default function Highlight() {
 
             <div className="masonry">
                 <div className="left-column ml-32">
-                    <div className="">
+                    <div className=" p-3 rounded-xl transition-all duration-300 hover:shadow-[0_0_20px_#00ffff]">
                         <img src={'/images/laptop.png'} alt="Laptop"/>
                         <p>Fly through demanding tasks up to 9.8x faster.</p>
                     </div>
 
-                    <div>
+                    <div className="p-10 rounded-xl transition-all duration-300 hover:shadow-[0_0_20px_#00ffff]">
                         <img src={'/images/sun.png'} alt="Sun"/>
                         <p>A stunning <br/>
                             Liquid Retina XDR <br/>
@@ -50,14 +50,14 @@ export default function Highlight() {
                 </div>
 
                 <div className="right-column mr-32">
-                    <div className="apple-gradient">
+                    <div className="apple-gradient rounded-2xl transition-all duration-300 hover:shadow-[0_0_20px_#00ffff]">
                         <img src={'/images/ai.png'} alt="AI"/>
                         <p>Built for <br/>
                             <span>Apple Intelligence.</span>
                         </p>
                     </div>
 
-                    <div>
+                    <div className="rounded-xl transition-all duration-300 hover:shadow-[0_0_20px_#00ffff]">
                         <img src={'/images/battery.png'} alt="Battery"/>
                         <p>
                             Up to 
